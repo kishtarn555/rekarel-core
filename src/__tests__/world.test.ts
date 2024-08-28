f46a2bfa54da7ff154336a937a0553cf42749fc9
kishtarn555 <hec_montemoon@hotmail.com>
2024-08-28 03:00:16 -0600

    [Test] Test the World.save argument

---
+++ src/__tests__/world.test.ts
@@ -0,0 +1,29 @@
+import {  World } from "../index"
+
+describe("World save", ()=> {
+    test("Test save start vs current world", ()=> {
+        const world = new World(100, 100);
+        world.setBagBuzzers(50);
+        world.addWall(4,4, 3);
+        world.setBuzzers(1,1, 10);
+        const before_start = world.save("start");
+        const before_current = world.save("current");
+        expect(before_start).toEqual(before_current); // Check that current and start match at the beginning
+        world.runtime.load([
+            ['PICKBUZZER'], 
+            ['FORWARD'], 
+            ['LEAVEBUZZER']
+        ]);
+        world.runtime.start();
+        while (world.runtime.state.running)
+            world.runtime.step();
+        
+        
+        const after_start = world.save("start");
+        const after_current = world.save("current"); 
+        expect(after_start).not.toEqual(after_current); // Check that current and start shouldn't be the same
+        expect(before_start).toEqual(after_start); // Check that both start should be equal
+
+        
+    });
+});
