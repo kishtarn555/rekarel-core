f324912e9b11857f94b88ec4c41682262c1d2422
kishtarn555 <hec_montemoon@hotmail.com>
2024-08-27 21:20:27 -0600

    [Refactor] Add intermediate ts files for jison files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { detectLanguage, compile, javaParser, pascalParser } from "./deprecate.karel.js";
+import { javaCompiler, pascalCompiler, detectLanguage, compile } from "./compiler.js";
 import { World } from "./world";
 import { Runtime } from "./runtime";
+import { java2PascalTranspiler, pascal2JavaTranspiler } from "./transpiler.js";
 
 export {
-   World,
-  Runtime
+  compile,
+  detectLanguage,
+  javaCompiler,
+  java2PascalTranspiler,
+  pascalCompiler,
+  pascal2JavaTranspiler,
+  Runtime,
+  World,
 };
