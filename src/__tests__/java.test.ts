60a3eeea68222a4c7d1213bc553da39ef8b8a5de
kishtarn555 <hec_montemoon@hotmail.com>
2024-08-28 16:25:54 -0600

    [Import] Add first import test

--- src/__tests__/java.test.ts
+++ src/__tests__/java.test.ts
@@ -1,11 +1,20 @@
 import { compile } from "../index"
 
 import fs from "fs"
+const sourceFiles = ["turnoff.kj", "importAll.kj"]
 describe("Java compilation tests ", ()=> {
     test("Test simple turnoff", ()=> {
         const turnoffJava = fs.readFileSync(__dirname +"/kj/turnoff.kj").toString();
         const result = compile (turnoffJava);
         expect(result).toEqual([ [ 'LINE', 3 ], [ 'HALT' ], [ 'LINE', 5 ], [ 'HALT' ] ])
+        
+    });
+    test("Test that codes compile correctly", ()=> {
+        for (const file of sourceFiles) {
+            const source = fs.readFileSync(__dirname +"/kj/"+file).toString();
+            const result = compile (source);
+            expect(Array.isArray(result)).toBe(true);
+        }
         
     });
 });
