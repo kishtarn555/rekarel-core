990eec808db2b650d1ebf66ad4b5efe77538776c
kishtarn555 <hec_montemoon@hotmail.com>
2024-08-29 12:35:18 -0600

    [Test][globals] Add globals simple unit testing

--- src/__tests__/java.test.ts
+++ src/__tests__/java.test.ts
@@ -1,7 +1,17 @@
-import { compile } from "../index"
+import { RawProgram } from "../compiler/opcodes";
+import { compile, World } from "../index"
 
 import fs from "fs"
-const sourceFiles = ["turnoff.kj", "importAll.kj"]
+const sourceFiles = ["turnoff.kj", "importAll.kj", "simpleFloor.kj", "simpleBackpack.kj", "globals.not.kj"]
+
+function runAll(world:World, code:RawProgram) {
+    world.runtime.load(code);
+    world.runtime.start();
+    while (world.runtime.state.running) {
+        world.runtime.step();
+    }
+}
+
 describe("Java compilation tests ", ()=> {
     test("Test simple turnoff", ()=> {
         const turnoffJava = fs.readFileSync(__dirname +"/kj/turnoff.kj").toString();
@@ -16,5 +26,58 @@ describe("Java compilation tests ", ()=> {
             expect(Array.isArray(result)).toBe(true);
         }
         
+    });
+});
+
+describe("Java globals test ", ()=> {
+    test("Test floor variable", ()=> {
+        const floorSrc = fs.readFileSync(__dirname +"/kj/simpleFloor.kj").toString();
+        const opcodes = compile (floorSrc);
+        expect(opcodes).toBeDefined()
+        const world = new World(10,10);
+        world.setBuzzers(1, 1, 5);
+        world.setBuzzers(3, 1, 2);
+        world.setBagBuzzers(20);
+        runAll(world, opcodes!);
+        expect(world.buzzers(2, 1)).toBe(5);
+        expect(world.buzzers(3, 1)).toBe(0);
+        expect(world.bagBuzzers).toBe(17);
+        
+    });
+    test("Test backpack variable", ()=> {
+        const backpackSrc = fs.readFileSync(__dirname +"/kj/simpleBackpack.kj").toString();
+        const opcodes = compile (backpackSrc);
+        expect(opcodes).toBeDefined()
+        const world = new World(25,25);
+        world.setBagBuzzers(12);
+        runAll(world, opcodes!);
+        expect(world.i).toBe(13);
+        expect(world.j).toBe(1);
+        expect(world.buzzers(13, 1)).toBe(12);
+        expect(world.bagBuzzers).toBe(0);
+        
+    });
+    test("Test shadowing compilation error", ()=> {
+        const backpackSrc = fs.readFileSync(__dirname +"/kj/globals.ce.kj").toString();
+        expect(()=>compile (backpackSrc)).toThrow(
+            /^Cannot name a parameter as a global variable/
+        );
+    });
+    test("Test shadowing compilation error", ()=> {
+        const src = fs.readFileSync(__dirname +"/kj/globals.ce.kj").toString();
+        expect(()=>compile (src)).toThrow(
+            /^Cannot name a parameter as a global variable/
+        );
+    });
+    test("Test the compatibility when not importing", ()=> {
+        const src = fs.readFileSync(__dirname +"/kj/globals.not.kj").toString();
+        const opcodes = compile (src);
+        expect(opcodes).toBeDefined()
+        const world = new World(25,25);
+        world.setBagBuzzers(120);
+        world.setBuzzers(1,1,10);
+        runAll(world, opcodes!);
+        expect(world.buzzers(1, 1)).toBe(14);
+        expect(world.bagBuzzers).toBe(116);
     });
 });
