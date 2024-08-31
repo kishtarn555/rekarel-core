bf99e84886c4053f1d637e036730a8fe2b5dca1f
kishtarn555 <hec_montemoon@hotmail.com>
2024-08-30 22:36:30 -0600

    [Test] Add and imporve LRET and SRET tests

--- src/__tests__/runtime.test.ts
+++ src/__tests__/runtime.test.ts
@@ -15,14 +15,40 @@ describe("Test runtime", () => {
             ["SRET"]
         ]);
         runtime.next();
+            expect(runtime.state.error).toBeUndefined(); 
         runtime.next();
-        expect(runtime.state.ret).toBe(12);
+            expect(runtime.state.error).toBeUndefined(); 
+            expect(runtime.state.ret).toBe(12);
+            expect(runtime.state.sp).toBe(-1);
+            runtime.next();
+            expect(runtime.state.error).toBeUndefined(); 
+            expect(runtime.state.ret).toBe(12);
+            runtime.next();
+            expect(runtime.state.error).toBeUndefined(); 
+            expect(runtime.state.ret).toBe(10);
+            expect(runtime.state.sp).toBe(-1);
+        });
+        
+        test("Test LRET", () => {
+        const world = new World(10,10)
+        const runtime = new Runtime(world);
+        world.runtime = runtime;
+        runtime.load([
+            ["LOAD", 12],
+            ["SRET"],
+            ["LOAD", 1],
+            ["LRET"]
+        ]);
         runtime.next();
-        expect(runtime.state.ret).toBe(12);
+        expect(runtime.state.error).toBeUndefined();  
         runtime.next();
-        expect(runtime.state.ret).toBe(10);
-
-
-
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();
+        expect(runtime.state.ret).toBe(12);        
+        expect(runtime.state.stack[0]).toBe(1);
+        expect(runtime.state.stack[1]).toBe(12);    
     });
 });
