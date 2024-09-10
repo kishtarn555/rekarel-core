e86d5d490ec317fd6f21c4b21bc791eba2e5a13c
kishtarn555 <hec_montemoon@hotmail.com>
2024-09-10 12:28:56 -0600

    [Test] Add LT and LTE tests

--- src/__tests__/runtime.test.ts
+++ src/__tests__/runtime.test.ts
@@ -51,4 +51,118 @@ describe("Test runtime", () => {
         expect(runtime.state.stack[0]).toBe(1);
         expect(runtime.state.stack[1]).toBe(12);    
     });
+    
+    test("Test LT", () => {
+        const world = new World(10,10)
+        const runtime = new Runtime(world);
+        world.runtime = runtime;
+        runtime.load([
+            ["LOAD", 2],
+            ["LOAD", 5],
+            ["LT"],
+
+            ["POP"],            
+            ["LOAD", 5],
+            ["LOAD", 2],
+            ["LT"],
+
+            ["POP"],            
+            ["LOAD", 5],
+            ["LOAD", 5],
+            ["LT"],
+            
+        ]);
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        expect(runtime.state.stack[0]).toBe(1);
+        expect(runtime.state.sp).toBe(0);
+
+        
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();
+          
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.stack[0]).toBe(0);
+        expect(runtime.state.sp).toBe(0);
+        
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();
+          
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        expect(runtime.state.stack[0]).toBe(0);
+        expect(runtime.state.sp).toBe(0);
+        
+        expect(runtime.state.error).toBeUndefined();  
+    });
+
+    
+    
+    test("Test LTE",() => {
+        const world = new World(10,10)
+        const runtime = new Runtime(world);
+        world.runtime = runtime;
+        runtime.load([
+            ["LOAD", 2],
+            ["LOAD", 5],
+            ["LTE"],
+
+            ["POP"],            
+            ["LOAD", 5],
+            ["LOAD", 2],
+            ["LTE"],
+
+            ["POP"],            
+            ["LOAD", 5],
+            ["LOAD", 5],
+            ["LTE"],
+            
+        ]);
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        expect(runtime.state.stack[0]).toBe(1);
+        expect(runtime.state.sp).toBe(0);
+
+        
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();
+          
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.stack[0]).toBe(0);
+        expect(runtime.state.sp).toBe(0);
+        
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();
+          
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        runtime.next();
+        expect(runtime.state.error).toBeUndefined();  
+        expect(runtime.state.stack[0]).toBe(1);
+        expect(runtime.state.sp).toBe(0);
+        
+        expect(runtime.state.error).toBeUndefined();  
+    });
 });
