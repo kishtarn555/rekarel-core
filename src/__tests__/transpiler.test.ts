b2d03fd1dd65bbf8ccc2c275526f976797dffc23
kishtarn555 <hec_montemoon@hotmail.com>
2024-10-07 02:15:57 -0600

    [Test] Test transpiler stability

--- src/__tests__/transpiler.test.ts
+++ src/__tests__/transpiler.test.ts
@@ -1,10 +1,21 @@
 import { transpileCode } from "../index"
 
 import fs from "fs"
-describe("Java2Pascal tests ", ()=> {
-    test("Test simple turnoff", ()=> {
-        //FIXME
-        expect(1).toBe(1);
+describe("Transpiler tests ", ()=> {
+    test("Test stability", ()=> {
+        const allJava = fs.readFileSync(__dirname +"/kj/allCode.kj").toString();
+        const allPascal = fs.readFileSync(__dirname +"/kp/allCode.kp").toString();
+
+        const java_java = transpileCode(allJava, "java");
+        console.log(java_java)
+        const java_java_java = transpileCode(java_java, "java");
+        //The transpiler should be stable
+        expect(java_java).toBe(java_java_java);
+
+        const pascal_pascal = transpileCode(allPascal, "pascal");
+        const pascal_pascal_pascal = transpileCode(pascal_pascal, "pascal");
+        //The transpiler should be stable
+        expect(pascal_pascal).toBe(pascal_pascal_pascal);
         
     });
 });
